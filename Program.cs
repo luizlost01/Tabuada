12fb1d978f7111b5903391e6506520a99ed0acb0
Luiz Eduardo <luized_sparvoli@hotmail.com>
2023-05-23 16:45:57 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira Qual Tabuada Deseja Que Seja Exibida");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Mostrando agora a tabuada do Número {num}");
+        tabuada(num);
+
+
+
+        static void tabuada(int tab){
+            for (int i = 0; i <= 10; i++)
+            {
+                int result = tab * i;
+                Console.WriteLine($"{tab} x {i} = {result}");
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+
+
+}
